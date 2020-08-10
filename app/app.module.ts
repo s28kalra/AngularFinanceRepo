196bcc1cd8ce3cf448c8fbf746bcd33fd24d5ebd
shivamsaini006 <shivamsaini006@gmail.com>
2020-08-10 19:17:38 +0530

    User & Admin Login

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AdminLoginComponent } from "./admin-login/admin-login.component";
+import { UserLoginComponent } from "./user-login/user-login.component";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
