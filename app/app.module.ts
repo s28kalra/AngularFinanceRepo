a0e5425dd0903674c3663c53c3baa41ca9ac15eb
s28kalra <sagar.kalra@lntinfotech.com>
2020-08-22 10:58:57 +0530

    register done end to end

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from "./homepage/homepage.component";
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-list/product-detail/product-detail.component';
-
+import { HttpClientModule } from "@angular/common/http";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { RegisterComponent } from './register/register.component';
 import { CartComponent } from './cart/cart.component';
@@ -41,7 +41,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatTooltipModule
+    MatTooltipModule,
+    HttpClientModule
 
   ],
   
