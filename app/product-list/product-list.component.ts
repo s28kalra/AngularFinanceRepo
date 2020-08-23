905322f3cd49909587b755891e35af2f541fc404
krishnv <krishna.vats02@gmail.com>
2020-08-23 12:13:49 +0530

    product-list

--- app/product-list/product-list.component.ts
+++ app/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Product } from "../models/product";
 //import { Product } from "../shared/product.model";
 import { Router } from '@angular/router';
+import { ProductListService } from "../services/product-list.service";
 
 @Component({
   selector: 'app-product-list',
@@ -16,42 +17,48 @@ export class ProductListComponent implements OnInit {
 
   // ];
   selectedProduct: Product;
-  @Input() product: Product;
-  @Output()  productSelected=new EventEmitter<Product>(); 
-   products=new Array<Product>();
+  // @Input() product: Product;
+  // @Output()  productSelected=new EventEmitter<Product>(); 
+  //  products=new Array<Product>();
+  products: any;
 
 
-  constructor(private route:Router) {
-    var c1;
-    c1 = new Product();
-    c1.productId = 1;
-    c1.productImageSource = "assets/airPods.jpg";
-    c1.productName = "Air Pods";
-    c1.productDescription = "Amazingly easy to use, Air Pods combine intelligent design with breakthrough technology and crystal clear sound";
-    c1.productPrice =40000;
-    c1.productQuantity=1;
-    this.products.push(c1);
+  constructor(private route:Router,private service: ProductListService) {
+    // var c1;
+    // c1 = new Product();
+    // c1.productId = 1;
+    // c1.productImageSource = "assets/airPods.jpg";
+    // c1.productName = "Air Pods";
+    // c1.productDescription = "Amazingly easy to use, Air Pods combine intelligent design with breakthrough technology and crystal clear sound";
+    // c1.productPrice =40000;
+    // c1.productQuantity=1;
+    // this.products.push(c1);
 
-    c1 = new Product();
-    c1.productId = 2;
-    c1.productImageSource = "assets/dellInspironI5_5575.JPG";
-    c1.productName = "Dell Inspiron I5 5575";
-    c1.productDescription = "Available in Variety. Laptops W/ 10th Gen Intel® Core™ Processor. Shop Now! Customize your Tech. Laptops W/ 10th Gen Intel® Core™ Processor. Buy Now! Free MS Office Home. ";
-    c1.productPrice =52000;
-    c1.productQuantity=1;
-    this.products.push(c1);
+    // c1 = new Product();
+    // c1.productId = 2;
+    // c1.productImageSource = "assets/dellInspironI5_5575.JPG";
+    // c1.productName = "Dell Inspiron I5 5575";
+    // c1.productDescription = "Available in Variety. Laptops W/ 10th Gen Intel® Core™ Processor. Shop Now! Customize your Tech. Laptops W/ 10th Gen Intel® Core™ Processor. Buy Now! Free MS Office Home. ";
+    // c1.productPrice =52000;
+    // c1.productQuantity=1;
+    // this.products.push(c1);
     
-    c1 = new Product();
-    c1.productId = 3;
-    c1.productImageSource = "assets/iphoneXS.jpg";
-    c1.productName = "Iphone XS";
-    c1.productDescription = "5.8-inch Super Retina display (OLED) with HDR1, IP68 dust and water resistant (maximum depth of 2 metres up to 30 minutes)2";
-    c1.productPrice =60000;
-    c1.productQuantity=1;
-    this.products.push(c1);
+    // c1 = new Product();
+    // c1.productId = 3;
+    // c1.productImageSource = "assets/iphoneXS.jpg";
+    // c1.productName = "Iphone XS";
+    // c1.productDescription = "5.8-inch Super Retina display (OLED) with HDR1, IP68 dust and water resistant (maximum depth of 2 metres up to 30 minutes)2";
+    // c1.productPrice =60000;
+    // c1.productQuantity=1;
+    // this.products.push(c1);
    }
 
   ngOnInit(): void {
+    this.service.getAllProducts().subscribe(
+      data=>{
+        this.products=data;
+      }
+    )
   }
   onSelected(event:any,product:any){ 
     this.selectedProduct=product;
