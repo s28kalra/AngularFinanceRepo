b465e02e04323525ce7f5be0bbad0ae4215d44bd
krishnv <krishna.vats02@gmail.com>
2020-08-23 23:47:25 +0530

    cus-dashboard

--- app/customer-dashboard/view-transactions/view-transactions.component.ts
+++ app/customer-dashboard/view-transactions/view-transactions.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewtransactionsService } from "../../services/viewtransactions.service";
+import { CustomerInfo } from "../../models/customer-info";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-transactions',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-transactions.component.css']
 })
 export class ViewTransactionsComponent implements OnInit {
-
-  constructor() { }
+  transactions:any;
+  customerId=0;
+  constructor(private service : ViewtransactionsService, private route:Router) {
+    if(sessionStorage.getItem("customerId")!=null)
+    this.customerId=parseInt(sessionStorage.getItem("customerId"));
+    else{
+      this.route.navigateByUrl('/userLoginLink')
+    }
+   }
 
   ngOnInit(): void {
+    if(this.customerId>0){
+    this.service.getListOfTransactionsOfCustomer(this.customerId).subscribe(
+      data=>{
+        this.transactions=data;
+      }
+      )
+    }
+    else{
+      this.route.navigateByUrl('/userLoginLink');
+    }
   }
 
 }
