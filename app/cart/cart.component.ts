fe6d7eb41e609e8f147c37941afbcee65f9f6211
krishnv <krishna.vats02@gmail.com>
2020-08-26 00:18:24 +0530

    checkout done

--- app/cart/cart.component.ts
+++ app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from "../models/product";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -9,71 +10,44 @@ import { Product } from "../models/product";
 export class CartComponent implements OnInit {
 
   subTotal=0;
-  cart = new Array<Product>();
-  constructor() {
-    var c1;
-    c1 = new Product();
-    c1.productId = 1;
-    c1.productImageSource = "assets/airPods.jpg";
-    c1.productName = "Air Pods";
-    c1.productDescription = "Amazingly easy to use, Air Pods combine intelligent design with breakthrough technology and crystal clear sound";
-    c1.productPrice =40000;
-    c1.productQuantity=1;
-    this.cart.push(c1);
-
-    c1 = new Product();
-    c1.productId = 2;
-    c1.productImageSource = "assets/dellInspironI5_5575.JPG";
-    c1.productName = "Dell Inspiron I5 5575";
-    c1.productDescription = "Available in Variety. Laptops W/ 10th Gen Intel® Core™ Processor. Shop Now! Customize your Tech. Laptops W/ 10th Gen Intel® Core™ Processor. Buy Now! Free MS Office Home. ";
-    c1.productPrice =52000;
-    c1.productQuantity=1;
-    this.cart.push(c1);
-    
-    c1 = new Product();
-    c1.productId = 3;
-    c1.productImageSource = "assets/iphoneXS.jpg";
-    c1.productName = "Iphone XS";
-    c1.productDescription = "5.8-inch Super Retina display (OLED) with HDR1, IP68 dust and water resistant (maximum depth of 2 metres up to 30 minutes)2";
-    c1.productPrice =60000;
-    c1.productQuantity=1;
-    this.cart.push(c1);
-    
+  cartProduct=new Product();
+  customerId:any;
+  constructor(private route:Router) { 
+    if(sessionStorage.getItem("customerId")!=null)
+    this.customerId=parseInt(sessionStorage.getItem("customerId"));
+    else{
+      this.route.navigateByUrl('/userLoginLink');
+    }
 
   }
 
   ngOnInit(): void {
-    this.cart.forEach(element => {
-      this.subTotal+=element.productPrice*element.productQuantity;
-    });
+    this.cartProduct=JSON.parse(sessionStorage.getItem('selectedItem'));
+    this.cartProduct.productQuantity=1;
+    sessionStorage.setItem("selectedItemQty",JSON.stringify(this.cartProduct.productQuantity));
+    this.cartProduct.productQuantity=1;
+    alert(JSON.stringify(this.cartProduct));
+   
+      this.subTotal+=this.cartProduct.productPrice*this.cartProduct.productQuantity;
       
   }
 
-  findIndex(productId){
-    for(var i=0;i<this.cart.length;i++)
-      if(this.cart[i].productId===productId)
-        return i;
-    return -1;
-  }
 
   addQuantity(productId){
-    var index=this.findIndex(productId);
-    this.cart[index].productQuantity++;
-    this.subTotal+=this.cart[index].productPrice;
+    this.cartProduct.productQuantity++;
+    this.subTotal+=this.cartProduct.productPrice;
   }
 
   reduceQuantity(productId){
-    var index=this.findIndex(productId);
-    if(this.cart[index].productQuantity>1){
-      this.cart[index].productQuantity--;    
-      this.subTotal-=this.cart[index].productPrice;
+    if(this.cartProduct.productQuantity>1){
+      this.cartProduct.productQuantity--;    
+      this.subTotal-=this.cartProduct.productPrice;
     }
   }
 
   removeProduct(productId){
-    var index=this.findIndex(productId);
-    this.subTotal-=this.cart[index].productQuantity* this.cart[index].productPrice;
-    this.cart.splice(index,1);
+    this.subTotal-=this.cartProduct.productQuantity* this.cartProduct.productPrice;
+    sessionStorage.removeItem('selectedItem');
   }
 
 }
